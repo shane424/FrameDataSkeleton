0eb873950fc59ae58e08a182cc0fab5d853fb6db
Shane <mugyou2@gmail.com>
2014-05-03 18:18:05 -0400

    Rejected commit: Default

--- src/com/teddyb/streetFighter/EditionSelectModeHelper/MyActivity.java
+++ src/com/teddyb/streetFighter/EditionSelectModeHelper/MyActivity.java
@@ -2,14 +2,41 @@ package com.teddyb.streetFighter.EditionSelectModeHelper;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MyActivity extends Activity {
+
+
+
     /**
      * Called when the activity is first created.
      */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Button ryuBtn = (Button)findViewById(R.id.RyuBtn);
+
+        ryuBtn.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Ryu.class);
+                startActivity(intent);
+
+
+            }
+        });
+}
+
+    /*public void goKen(View v){
+        Log.d("Test", "KenPushed");
     }
+    public void goRyu(View v){
+        Log.d("Test", "RyuPushed");
+    }
+    */
 }
---
+++ src/com/teddyb/streetFighter/EditionSelectModeHelper/Ryu.java
@@ -0,0 +1,19 @@
+package com.teddyb.streetFighter.EditionSelectModeHelper;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class Ryu extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ryu);
+    }
+
+
+}

e6f489df6fc7bbf60121ee1910f2c111d4b34319
Tim Nguyen <Tim.H.Nguyen90@gmail.com>
2014-06-20 15:13:45 -0400

    rename of entire project

---
+++ src/com/teddyb/FrameDataSkeleton/Beowulf.java
@@ -0,0 +1,20 @@
+package com.teddyb.FrameDataSkeleton;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import com.teddyb.FrameDataSkeleton.R;
+
+public class Beowulf extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.beowulf);
+    }
+
+
+}
